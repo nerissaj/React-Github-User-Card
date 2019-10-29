515c9e90a8f4d3603bfe880b44b167b7a24cf884
Nerissa Taylor <taylornerissa@hotmail.com>
2019-10-29 18:15:29 -0400

    use class component, display and fetch data from github api

--- githubusercard-app/src/App.js
+++ githubusercard-app/src/App.js
@@ -1,54 +1,69 @@
 import React from 'react';
 import UsersCard from './UsersCard';
-import Search from './Users';
+import axios from 'axios';
+import FollowersCard from './FollowersCard';
 
 import './App.css';
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      userName: "nerissaj",
-      user:{},
-      followers:[]
+    this.state = {
+      user: '',
+      followers: []
     };
   }
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/nerissaj")
+      .then(response => {
+        //console.log(response.data);
+        this.setState({
+          user: response.data
+        });
+      })
+      .catch(err => console.log(err))
+  }
+  componentDidUpdate = () => {
 
-changeUserName = (userName) => {
-this.setState({userName});
-}
-fetchUser = () => {
-  fetch(`https://api.github.com/users/${this.state.userName}`)
-  .then(response => response.json())
-  .then(data => this.setState({user:data}));
+    axios
+      .get(`https://api.github.com/users/nerissaj/followers`)
+      .then(response => {
+        //console.log("response");
+        this.setState({ followers: response.data });
+      })
+      .catch(err => console.log(err));
+  }
 
-} 
-        fetchFollowers =() => {
-          fetch(`https://api.github.com/users/${this.state.userName}/followers`)
-          .then(res => res.json())
-          .then(data => this.setState({followers:data}));
-      }
-      componentDidMount(){
-        console.log("First Render (mounting)");
-        this.fetchUser();
-        this.fetchFollowers();
-    }
-    componentDidUpdate(prevProps, prevState){
-      console.log(this.state);
-      if(prevState.userName !== this.state.userName){
-        this.fetchUser();
-        this.fetchFollowers();
-
-      }
-    }
-  
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
-        <Search  changeUserName={this.changeUserName} />
-        <UsersCard user={this.state.user} followers={this.state.followers} />
+        {/* <Search  changeUserName={this.changeUserName} /> */}
+        <UsersCard user={this.state.user} />
+        <FollowersCard followers={this.state.followers} />
       </div>
-    );}
+    );
+  }
 }
- 
- 
+
+
 export default App;
+// componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.user != - this.state.user) {
+  //     if (this.state.user === 'nerissaj') {
+  //       axios
+  //         .get("https://api.github.com/users/")
+  //         .then(response => {
+  //           this.setState({
+  //             user: response.data,
+  //             followers: []
+  //           })
+  //         })
+  //         .catch(err => console.log(err));
+  //     }
+  //   }
+  //   if (prevProps.someValue !== this.props.someValue) {
+  //   }
+  // }
+  // handleChanges = event => {
+  //   this.setState({ user: event.target.value });
+  // }
--- githubusercard-app/src/UsersCard.js
+++ githubusercard-app/src/UsersCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 
-function UserCard(props){
-    return(  
-      <div>
-        <h2>{props.user.login}</h2>
-        <p>{props.user.location}</p>
-        <p>{props.user.url}</p>
-        <div>
-        {props.followers.map(follower => (<div key={follower.id}>{follower.login}</div>
-        ))}
-      </div>
+function UserCard(props) {
+  return (
+    <div className="user">
+      <img src={props.user.avatar_url} alt="github pic" />
+
+      <h2>{props.user.login}</h2>
+      <p>{props.user.location}</p>
+      <p>followers:{props.user.followers}</p>
+      <p>following:{props.user.following}</p>
+
     </div>
-    );
+  );
 }
-   
+
 
 
 
